093fedfdd8ff4aed32de36b08faf1294c2c5709a
Md. Emtiaz Zahid <emtiazzahid@gmail.com>
2016-06-07 16:13:19 +0600

    404 page edited

---
+++ 404.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>!!Page Under Construction!!</title>
+	<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<!-- //for-mobile-apps -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+	<style>
+		.error {
+  margin: 0 auto;
+  text-align: center;
+}
+
+.error-code {
+  bottom: 60%;
+  color: #2d353c;
+  font-size: 96px;
+  line-height: 100px;
+}
+
+.error-desc {
+  font-size: 12px;
+  color: #647788;
+}
+
+.m-b-10 {
+  margin-bottom: 10px!important;
+}
+
+.m-b-20 {
+  margin-bottom: 20px!important;
+}
+
+.m-t-20 {
+  margin-top: 20px!important;
+}
+	</style>
+</head>
+<body>
+	
+</body>
+</html>
+
+
+<div class="error">
+        <div class="error-code m-b-10 m-t-20">Oops!!  <i class="fa fa-warning"></i></div>
+        <h3 class="font-bold">The Page in Under Construction</h3>
+
+        <div class="error-desc">
+            Sorry, but the page you are looking for was either not found or does not exist. <br/>
+            Try refreshing the page or click the button below to go back to the Homepage.
+            <div>
+                <a class=" login-detail-panel-button btn" href="index.php">
+                        <i class="fa fa-arrow-left"></i>
+                        Go back to Homepage                        
+                    </a>
+            </div>
+        </div>
+    </div>
+
+ 
